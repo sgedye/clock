f7a7e4873e54e1f23e8f7d6132c6b3f9fedbbee1
Shaun Gedye <sgedye88@yahoo.com.au>
2021-02-21 01:14:43 +0800

    Update index.js
    Remove the rAF visual bug

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-const hourHand = document.querySelector('.hour-hand');
-const minuteHand = document.querySelector('.minute-hand');
-const secondHand = document.querySelector('.second-hand');
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const secondHand = document.querySelector(".second-hand");
 
-(function createLines() {
-  const lines = document.querySelector('.lines');
+let seconds, minutes, hourDegrees;
+
+(() => {
+  // Create the lines delimiating the 60 marks of a clock face
+  const lines = document.querySelector(".lines");
   for (let i = 0; i < 60; i++) {
-    const line = document.createElement('div');
-    line.classList.add('line')
+    const line = document.createElement("div");
+    line.classList.add("line");
     line.style.transform = `rotate(${i * 6}deg)`;
-    lines.appendChild(line)
+    lines.appendChild(line);
   }
-})()
 
-function getCurrentTime() {
-  const currentTime = new Date();
-  const hours = currentTime.getHours() / 12 * 360 + 90;
-  const minutes = currentTime.getMinutes() / 60 * 360 + 90;
-  const seconds = currentTime.getSeconds() / 60 * 360 + 90;
-  hourHand.style.transform = `rotate(${hours}deg)`;
-  minuteHand.style.transform = `rotate(${minutes}deg)`;
-  secondHand.style.transform = `rotate(${seconds}deg)`;
-}
-setInterval(getCurrentTime, 1000);
+  // Setting the initial time (note hours step by degrees, not units)
+  const initialTime = new Date();
+  const hours = initialTime.getHours() % 12;
+  minutes = initialTime.getMinutes();
+  seconds = initialTime.getSeconds();
+  hourDegrees = hours * 30 + minutes * 0.5;
+
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${6 * minutes}deg)`;
+  secondHand.style.transform = `rotate(${6 * seconds}deg)`;
+
+  secondHand.style.opacity = 1;
+  minuteHand.style.opacity = 1;
+  hourHand.style.opacity = 1;
+})();
+
+// Update the clock every 1000ms (approx.)
+setInterval(() => {
+  seconds++;
+  secondHand.style.transform = `rotate(${6 * seconds}deg)`;
+
+  if (seconds % 60 === 0) {
+    minutes++;
+    hourDegrees += 0.5;
+    minuteHand.style.transform = `rotate(${6 * minutes}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  }
+}, 1000);
